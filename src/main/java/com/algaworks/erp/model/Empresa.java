27c758d48d5d4c72919d5f984c59f537c9a9ffbc
yuji-exe <119537277+yuji-exe@users.noreply.github.com>
2023-10-04 02:07:04 -0300

    • Modified Empresa.java (added faturamento) • Added faces-config.xml • Modified index.xhtml (added requirements, validators, converters, messages and faturamento)
    {Front tested and working}
    {Components working as expected}

--- src/main/java/com/algaworks/erp/model/Empresa.java
+++ src/main/java/com/algaworks/erp/model/Empresa.java
@@ -2,6 +2,7 @@ package com.algaworks.erp.model;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Objects;
 
@@ -33,6 +34,17 @@ public class Empresa implements Serializable {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 30)
     private TipoEmpresa tipo;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal faturamento;
+
+    public BigDecimal getFaturamento() {
+        return faturamento;
+    }
+
+    public void setFaturamento(BigDecimal faturamento) {
+        this.faturamento = faturamento;
+    }
 
     public TipoEmpresa getTipo() {
         return tipo;
